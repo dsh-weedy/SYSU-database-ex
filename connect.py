ea4a9236e918e7d0b77a3ad4e94065fee6bcf235
fromweaktostrong <114817987+fromweaktostrong@users.noreply.github.com>
2023-12-10 17:30:53 +0800

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,4 +1,13 @@
-import numpy
+import pymysql
 
-hoodja 
-shdoadjasd
+def connect_db():
+    db = pymysql.connect(host = "localhost", user = "root", password = "123456",database = "car", port = 3306)
+    return db
+
+db = connect_db()
+sql = "select * from customer"
+cursor = db.cursor()
+cursor.execute(sql)
+
+result = cursor.fetchall()
+print(result)
