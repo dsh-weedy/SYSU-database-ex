9bd51de3629be05eb343ee11140c08cf9fde311c
fromweaktostrong <114817987+fromweaktostrong@users.noreply.github.com>
2023-12-11 11:16:57 +0800

    Update inquire.py

--- inquire.py
+++ inquire.py
@@ -1,7 +1,11 @@
 import pymysql
 import connect
 
-# use usr_id to inquire usr_order
+READY = 1
+RENT = 2
+SALE = 3
+
+# use usr_id to inquire usr_purchase_id usr_lease_id
 def inquire_usr_order(usr_id):
     # create connetion and cursor
     db = connect.connect_db() 
@@ -10,34 +14,109 @@ def inquire_usr_order(usr_id):
     # inquire purchase list
     sql = "select *\
           from purchase \
-          where cus_id = %s" %(usr_id)
+          where usr_id = %s"
     
-    cursor.execute(sql)
-    result_purchase = cursor.fetchall()
+    # inquire
+    values = (usr_id)
+    cursor.execute(sql,values)
+    usr_purchase = cursor.fetchall()
 
     # inquire lease list
-    sql = "select *\
+    sql = "select * \
         from lease \
-        where usr_id = %s" %(usr_id)
+        where usr_id = %s" 
     
-    cursor.execute(sql)
-    result_lease = cursor.fetchall()
+    # inquire
+    values = (usr_id)
+    cursor.execute(sql,values)
+    usr_lease = cursor.fetchall()
 
-    return result_purchase,result_lease
+    db.close()
+    # return usr_purchase_id usr_lease_id
+    return usr_purchase,usr_lease
 
-# inquire all item in car, return car_id
+# inquire all item in car, return car
 def inquire_car_all():
     # create connetion and cursor
     db = connect.connect_db() 
     cursor = db.cursor() 
 
     # inquire car list
-    sql = "select car_id\
+    sql = "select *\
           from car "
     
     cursor.execute(sql)
-    result_car_id = cursor.fetchall()
-    return result_car_id
+    result_car = cursor.fetchall()
 
+    db.close()
+    return result_car
+
+# inquire car which is avlible return car_id
+def inquire_car_avlible():
+    # create connetion and cursor
+    db = connect.connect_db() 
+    cursor = db.cursor() 
+
+    # inquire car which is avlible
+    sql = "select *\
+          from car \
+          where state = %s"
+    
+    values = (READY)
+    cursor.execute(sql,values)
+    result_car_avlible = cursor.fetchall()
+
+    db.close()
+    return result_car_avlible
+
+
+# inquire all item in employees, return emp
+def inquire_emp_all():
+    # create connetion and cursor
+    db = connect.connect_db() 
+    cursor = db.cursor() 
+
+    # inquire emp list
+    sql = "select *\
+          from emp "
+    
+    cursor.execute(sql)
+    result_emp = cursor.fetchall()
+    
+    db.close()
+    return result_emp
+
+# inquire all item in car, return car with particular car_id
+def inquire_car_detail(car_id):
+    # create connetion and cursor
+    db = connect.connect_db() 
+    cursor = db.cursor() 
+
+    # inquire emp list
+    sql = "select * from car where car_id = %s"
+    values = (car_id)
+    
+    cursor.execute(sql,values)
+    result_car = cursor.fetchall()
+
+    db.close()
+    return result_car
+
+
+# inquire all item in car, return car with particular car_id
+def inquire_usr_detail(usr_id):
+    # create connetion and cursor
+    db = connect.connect_db() 
+    cursor = db.cursor() 
+
+    # inquire usr list
+    sql = "select * from usr where usr_id = %s"
+    values = (usr_id)
+    
+    cursor.execute(sql,values)
+    result_usr = cursor.fetchall()
+
+    db.close()
+    return result_usr
 
 
