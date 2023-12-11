84682e91c001516026561576bf20154fee7a7a8c
Trusin Catherine <zhengbch5@mail2.sysu.edu.cn>
2023-12-11 14:29:17 +0800

    update 4 check_func

--- code/test.py
+++ code/test.py
@@ -2,4 +2,5 @@ import pymysql
 import func
 
 conn = pymysql.connect(host='localhost', user='root', password='@Zbc0038;', database='sysu_database', port=3306)
-print(func.usrlogin(2, '123456', conn))
+print(func.check_cus_detail(3, conn))
+
